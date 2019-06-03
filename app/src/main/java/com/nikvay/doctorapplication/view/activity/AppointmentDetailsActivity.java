b998a6902cac677e9f585964b8f9e984de89fbdd
Nikvay-pc1 <socialnikvay@gmail.com>
2019-06-03 10:26:07 +0530

    NJ Commit 1 3-06-2019

---
+++ app/src/main/java/com/nikvay/doctorapplication/view/activity/AppointmentDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.nikvay.doctorapplication.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nikvay.doctorapplication.R;
+
+public class AppointmentDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_details);
+    }
+}
