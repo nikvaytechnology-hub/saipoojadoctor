d4b4c62ccaf03777cc004f41da269ca980937435
Nikvay-pc1 <socialnikvay@gmail.com>
2019-07-11 20:08:07 +0530

    Nj 8 11-07-2019

--- app/src/main/java/com/nikvay/doctorapplication/apicallcommon/EndApi.java
+++ app/src/main/java/com/nikvay/doctorapplication/apicallcommon/EndApi.java
@@ -1,5 +1,4 @@
 package com.nikvay.doctorapplication.apicallcommon;
-
 public class EndApi extends BaseApi {
     public static final String LOGIN = "ws-login";
     public static final String SERVICE_LIST = "ws-service";
